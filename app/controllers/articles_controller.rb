9611d8eda9ab1accd0a122fb1adb684bd245bd76
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-11-12 20:26:34 +0100

    .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "nayed", password: "0000", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
